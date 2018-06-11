afb2bd6c688e5f3037bf1cb0e26bf7db73892bdb
Digambar <digambar.malla@gmail.com>
2018-06-11 16:57:14 +0530

    Commit-2

--- service.py
+++ service.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 import psycopg2
-import pymssql
-import os
-import uuid
-import csv
-import boto3
-from botocore.client import Config
+import pandas as pd
+from psycopg2.extras import RealDictCursor
 
 def handler(event, context):
     ret = {
@@ -14,30 +10,48 @@ def handler(event, context):
     }
 
     #get input value
-    dbhost = event.get("dbhost")
-    dbname = event.get("dbname")
-    dbuser = event.get("dbuser")
-    dbpassword = event.get("dbpassword")
-    dbquery = event.get("dbquery")
+    dbhost = "ivy-dev.chwrf3ujkohq.ap-southeast-1.redshift.amazonaws.com"
+    dbname = "dev_png"
+    dbuser = "admin"
+    dbpassword = "Ivy12345"
     dbtype = event.get("dbtype","redshift")
 
-    
-    return ret
-
-
-# connect to database and fetch data
-def getdatafromdb(dbname,dbhost,dbuser,dbpassword,dbquery):
+    #Open Connection
     print('connecting to database...')
     conn = psycopg2.connect(dbname=dbname, host=dbhost, port= '5439', user= dbuser, password= dbpassword)
     print('connection successful')
 
-    #execute query
-    curr = conn.cursor()
-    curr.execute(dbquery)
-    recs = curr.fetchall()
-    
-    # close connection and cursor
-    curr.close()
+    #Stock Validation Process--- Need to check org_id
+    dbquery = "select * from Config_Application where config_type='SALES_ORDER' AND config_key='ORDER_ON_PARENT_SKU'"  
+    ConfigApp_data = getdatafromdb(conn,dbquery)
+
+
+
+    #Scheme Process
+
+    #Discount Details
+
+    #Tax Details
+
+    #Other Changes
+
     conn.close()
-    return recs    
+    return ret
+
+
+# connect to database and fetch data
+def getdatafromdb(conn,dbquery):    
+
+    #execute query
+    curr = conn.cursor(cursor_factory=RealDictCursor)
+    curr.execute(dbquery)
+    df = pd.DataFrame(curr.fetchall())
+    #df.columns = resoverall.keys()
+
+    #Another way of fetching data
+    #df = pd.read_sql(dbquery, con=conn)
+    
+    # close cursor
+    curr.close()    
+    return df    
 
